6768bb182eca13724d4afc7c4f6d7fd0a076738a
KhesrawFarid <khesrawfarid@gmail.com>
2026-04-22 16:29:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/BlitzPlayGame/',
+
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
